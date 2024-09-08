fb3e0c0802149a13958f8d17d54e714e8d1bbfb0
den2k6 <den.2k6aug17@gmail.com>
2024-09-09 05:50:18 +0900

    Minor corrections2

--- selenium_on_docker.py
+++ selenium_on_docker.py
@@ -14,9 +14,9 @@ def test_selenium(url, expected_h1):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--window-size=1920,1080")
     # chrome_options.add_argument("--remote-debugging-port=9222")
 
     driver = webdriver.Chrome(options=chrome_options)
