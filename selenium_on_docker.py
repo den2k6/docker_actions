277c6ff8d5d6faa0bd04d59d5adbd0669664f872
den2k6 <den.2k6aug17@gmail.com>
2024-09-11 21:46:23 +0900

    edit

--- selenium_on_docker.py
+++ selenium_on_docker.py
@@ -24,11 +24,13 @@ def test_selenium(url, expected_h1):
     # exec selenium
     driver.get(url)
     h1_text = driver.find_element(By.TAG_NAME, "h1").text
+    h2_text = driver.find_element(By.TAG_NAME, "h2").text
     assert (
         h1_text == expected_h1
     ), f"h1 text does not match. Expected: {expected_h1}, Actual: {h1_text}"
     print(f"  title:   {driver.title}")
     print(f"  h1 text: {h1_text}")
+    print(f"  h2 text: {h2_text}")
 
     driver.quit()
 
